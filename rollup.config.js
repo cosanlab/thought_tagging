0e82fd9da0593f485481171a4af8abcb154a7e29
ejolly <eshin.jolly@gmail.com>
2019-10-22 14:42:19 -0400

    Added packages to ensure compatibility across the majority of web-browsers.

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import postcss from 'rollup-plugin-postcss';
 import replace from 'rollup-plugin-replace';
 import commonjs from 'rollup-plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
+import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 const production = !process.env.ROLLUP_WATCH;
@@ -43,6 +44,27 @@ export default {
         // but let Rollup handle all other warnings normally
         handler(warning);
       }
+    }),
+    // Babel is being used to make ES2015 features (like arrow functions) available to all browsers. This is called *transpiling*. Additionally babel using using core-js to create *pollyfills* to make turn some JS features that don't exist in some browsers into compatible ones that do exist. This has to increase bundle size a bit but that's the cost of higher compatibility. Refs: 
+    // http://simey.me/svelte3-rollup-and-babel7/
+    // https://github.com/sveltejs/svelte/issues/3388
+    // https://github.com/sveltejs/svelte/issues/2621
+    // https://blog.az.sg/posts/svelte-and-ie11/
+    babel({
+      extensions: [".js", ".mjs", ".html", ".svelte"],
+      include: ['src/**', 'node_modules/svelte/**'],
+      runtimeHelpers: true,
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            "debug": true,
+            useBuiltIns: 'usage',
+            corejs: 3,
+            targets: "> 0.25%, not dead"
+          },
+        ],
+      ],
     }),
     postcss(),
 
